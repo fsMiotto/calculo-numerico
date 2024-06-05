56ddaf99e923fc4e556026eb8946037a433b73ba
fsMiotto <felipe1miotto@gmail.com>
2024-06-05 15:51:43 -0300

    codigo final1

--- Trab1_CN.c
+++ Trab1_CN.c
@@ -8,49 +8,53 @@
 
 //Lendo o arquivo
 void ler_matriz(double matriz[MAX][MAX], const char *nome_arquivo) {
+    //Abrindo o arquivo
     FILE *arquivo = fopen(nome_arquivo, "r");
+
+    //Erro
     if (!arquivo) {
         printf("Arquivo nao encontrado");
         exit(0);
     }
 
+    //Lendo o arquivo
     for (int i=0; i < MAX; i++) {
         for (int j=0; j < MAX; j++) {
             fscanf(arquivo, "%lf", &matriz[i][j]);
         }
     }
+
+    //Fechando o arquivo
     fclose(arquivo);
 }
 
 //Teste de Parada
-int teste_parada(double *lambda, double *autovalor) {
+int teste_parada(double *autovalor_0, double *autovalor_1) {
     double erro, menor_erro = 100;
-    int i_final;
+    int i_dominante;
     
     //Obtendo o menor erro da iteracao
     for (int i=0; i < MAX; i++) {
-        erro = fabs(autovalor[i] - lambda[i]) / fabs(autovalor[i]);
+        //Formula do erro
+        erro = fabs(autovalor_1[i] - autovalor_0[i]) / fabs(autovalor_1[i]);
+        //obtendo o menor erro para comparar com a parada
         if (erro < menor_erro) {
             menor_erro = erro;
-            i_final = i;
+            i_dominante = i;
         }
     }
     
     //Comparando os erros
-    if (menor_erro >= epsilon) {
-        for (int i =0; i<MAX; i++){
-            lambda[i] = autovalor[i];   
-        }
-        return -1;
-    }
-    return i_final;
+    if (menor_erro >= epsilon) {return -1;} //continua o metodo
+    else {return i_dominante;} //para e retorna a posição do autovalor com menor erro
 }
 
 //Eliminacao de gauss sem pivotamento
-void metodo_eliminacao_gauss (double R[MAX][MAX], double Z[MAX], double Y[MAX]) {
+void metodo_eliminacao_gauss (double R[MAX][MAX], double *Z, double *Y) {
     double Yaux[MAX],Raux[MAX][MAX];
     double fator, max;
 
+    //criando clones de Y e R para usar no metodo
     for(int i = 0; i < MAX; i++){
         Yaux[i] = Y[i];
         for (int j = 0; j < MAX; j++){
@@ -58,7 +62,7 @@ void metodo_eliminacao_gauss (double R[MAX][MAX], double Z[MAX], double Y[MAX]) 
         }
     }
     
-    //eliminacoes
+    //realizando as eliminacoes
     for (int i=0; i < MAX; i++) {
         //matriz triangular superior
         for (int j = i+1; j < MAX; j++) {
@@ -70,7 +74,7 @@ void metodo_eliminacao_gauss (double R[MAX][MAX], double Z[MAX], double Y[MAX]) 
         }
     }
 
-    // substituicao
+    // fazendo as substituicao
     for(int i = MAX-1; i >= 0; i--){
         for (int j = MAX-1; j > i; j--){
             Yaux[i] -= Raux[i][j]*Z[j];
@@ -80,22 +84,26 @@ void metodo_eliminacao_gauss (double R[MAX][MAX], double Z[MAX], double Y[MAX]) 
 }
 
 //Metodo das potencias
-int metodo_potencias(double matriz[MAX][MAX], double *autovalor, double *autovetor, double *Y) {
-    double Z[MAX], lambda[MAX], norma;
-    int iter = 0, iter_final;
+double metodo_potencias(double R[MAX][MAX], double *autovalor_1, double *autovetor, double *Y) {
+    double Z[MAX], autovalor_0[MAX], Yaux[MAX];
+    double norma;
+    int iter = 0, i_dominante;
     
     //valores iniciais
     for (int i=0; i < MAX; i++) {
-        lambda[i] = 0;
+        //auxiliar de Y para não alterar o original
+        Yaux[i] = Y[i];
         autovetor[i] = 1;
     }
     
     do {
-        // multiplicacao matriz-vetor
+        // multiplicando matriz com autovetor
         for (int i=0; i < MAX; i++) {
+            //zerando o vetor Z
             Z[i] = 0;
             for (int j=0; j < MAX; j++) {
-                Z[i] += matriz[i][j] * autovetor[j];
+                //atualizando o vetor Z
+                Z[i] += R[i][j] * autovetor[j];
             }
         }
     
@@ -108,66 +116,75 @@ int metodo_potencias(double matriz[MAX][MAX], double *autovalor, double *autovet
         }
 
         for (int i=0; i < MAX; i++) {
+            //atualizando o autovalor(k-1)
+            autovalor_0[i] = autovalor_1[i];
             //atualizanado autovetor
             autovetor[i] = Z[i] / norma;
-            //atualizar autovalor
-            autovalor[i] = Z[i] / Y[i];
+            //atualizar autovalor(k)
+            autovalor_1[i] = Z[i] / Yaux[i];
             //atualizando Y(k)
-            Y[i] = autovetor[i];
+            Yaux[i] = autovetor[i];
+            
         }
 
         //teste de parada
-        iter_final = teste_parada(lambda, autovalor);
+        i_dominante = teste_parada(autovalor_0, autovalor_1);
         iter++;
+        
+        } while ( iter < MAX_iter && i_dominante == -1);
 
-        } while ( iter < MAX_iter || iter_final == -1);
-    return iter_final;
+    return autovalor_1[i_dominante]; //Retorna o autovalor dominante
 }
 
 //Metodo das potencias inverso
-int metodo_potencias_inverso(double matriz[MAX][MAX], double *autovalor, double *autovetor, double *Y, double deslocamento) {
-    double A[MAX][MAX], Z[MAX], lambda[MAX];
+double metodo_potencias_inverso(double R[MAX][MAX], double *autovalor_1, double *autovetor, double *Y, double deslocamento) {
+    double A[MAX][MAX], Z[MAX], autovalor_0[MAX], Yaux[MAX];
     double norma;
-    int iter = 0, iter_final;
+    int iter = 0, i_dominante;
     
     //valores iniciais
-    for (int i = 0; i < MAX; i++) {
-        Y[i] = 1.0;
-        lambda[i] = 0.0;
+    for (int i=0; i < MAX; i++) {
+        //auxiliar de Y para não alterar o original
+        Yaux[i] = Y[i];
+        //Montando uma matriz R para fazer o deslocamento
         for(int j=0; j < MAX; j++){
-            A[i][j] = matriz[i][j];
+            A[i][j] = R[i][j];
         }
+        //Fazendo o deslocamento
         A[i][i] -= deslocamento;
     }
     
     do {
         //atualizar vetor Z
-        metodo_eliminacao_gauss(A, Z, Y);
+        metodo_eliminacao_gauss(A, Z, Yaux);
         
         //obtendo a norma
         norma = fabs(Z[0]);
-        for (int i = 0; i < MAX; i++) {
+        for (int i=0; i < MAX; i++) {
             if (fabs(Z[i]) > norma) {
                 norma = fabs(Z[i]);
             }
         }
 
-        for (int i = 0; i < MAX; i++) {
+        for (int i=0; i < MAX; i++) {
+            //atualizando o autovalor(k-1)
+            autovalor_0[i] = autovalor_1[i];
             //atualizar autovetor
             autovetor[i] = Z[i] / norma;
-            //atualizar autovalor
-            autovalor[i] = Z[i] / Y[i];
+            //atualizar autovalor(k)
+            autovalor_1[i] = Z[i] / Yaux[i];
             //atualizando Y(k)
-            Y[i] = autovetor[i];
-        }
+            Yaux[i] = autovetor[i];
+            }
 
         //teste de padara
-        iter_final = teste_parada(lambda, autovalor);
+        i_dominante = teste_parada(autovalor_0, autovalor_1);
         iter++;
 
-        } while ( iter < MAX_iter || iter_final == -1);
-        
-    return iter_final;
+        } while ( iter < MAX_iter && i_dominante == -1);
+
+    //Retorna o autovalor dominante  
+    return (1/autovalor_1[i_dominante]) + deslocamento; 
 }
 
 
@@ -178,8 +195,10 @@ int main(){
     //Declaração de vetores
     double autovalor[MAX], autovetor[MAX], Y[MAX], deslocamento[MAX];
     //declaração de variaveis
-    int iter_final, m = 0;
+    double autovalor_dominante;
+    int m = 0;
     
+    //Obtendo a matriz R
     ler_matriz(R, "correlacao.txt");
 
     //zerando a matriz D
@@ -197,14 +216,15 @@ int main(){
 
 
     // aplicando o metodo das potencias
-    iter_final = metodo_potencias(R, autovalor, autovetor, Y);
-    printf("\nPelo metodo das potencias R possui: \nAutovalor dominante:  %lf \nAutovetor correspondente:\n", autovalor[iter_final]);
+    autovalor_dominante = metodo_potencias(R, autovalor, autovetor, Y);
+    printf("\nPelo metodo das potencias R possui: \nAutovalor dominante:  %lf \nAutovetor correspondente:\n", autovalor_dominante);
+    //printando o autovetor
     for (int i = 0; i < MAX; i++){
         printf("%lf\n", autovetor[i]);
     }
     
     // Para o metodo de kaiser
-    if(autovalor[iter_final] > 1 ){m++;}
+    if(autovalor_dominante > 1 ){m++;}
     
     // MATRIZES D e P
     for (int j=0; j < MAX; j++) {
@@ -214,7 +234,7 @@ int main(){
         D[0][j] = 0;
     }
     // autovalor dominante na primeira posição da matriz d autovalores
-    D[0][0] = autovalor[iter_final];
+    D[0][0] = autovalor_dominante;
 
 
 
@@ -222,17 +242,17 @@ int main(){
     printf("insira os (6) deslocamentos para o metodo das potencias em ordem decrescente (sepados por espaco)\n");
     for (int i=0; i < MAX-1; i++){
         scanf("%lf", &deslocamento[i]);
-        while(deslocamento[i] < 0 || deslocamento[i] > autovalor[iter_final]){
-            printf("valor invalido, digite um valor entre 0 e %lf:  ", autovalor[iter_final]);
+        //verificando se é valido
+        while(deslocamento[i] < 0 || deslocamento[i] > autovalor_dominante){
+            printf("valor invalido, digite um valor entre 0 e %lf:  ", autovalor_dominante);
             scanf("%lf", &deslocamento[i]);
         }
     }
 
     //aplicando o metodo das potencias inverso com deslocamento
     for(int i=0; i < MAX-1; i++){
-        iter_final = metodo_potencias_inverso(R, autovalor, autovetor, Y, deslocamento[i]);
-        double autovalor_inverso = (1/autovalor[iter_final]) + deslocamento[i];
-        printf("\n\nPelo metodo das potencias inverso com deslocamento (%.3lf), R possui: \nAutovalor: %lf \nAutovetor correspondente:\n", deslocamento[i], autovalor_inverso);
+        autovalor_dominante = metodo_potencias_inverso(R, autovalor, autovetor, Y, deslocamento[i]);
+        printf("\n\nPelo metodo das potencias inverso com deslocamento (%.3lf), R possui: \nAutovalor: %lf \nAutovetor correspondente:\n", deslocamento[i], autovalor_dominante);
 
         //Matriz P e D
         for (int j=0; j < MAX; j++) {
@@ -243,10 +263,10 @@ int main(){
             D[i+1][j] = 0; 
         }
         // Obtendo o autovalor da diagonal de D
-        D[i+1][i+1] = autovalor_inverso;
+        D[i+1][i+1] = autovalor_dominante;
 
         //achando o "m" para o criterio de kaiser
-        if(autovalor_inverso > 1 ){m++;}
+        if(autovalor_dominante > 1 ){m++;}
     }
     
     //Prints da matris D
@@ -267,7 +287,8 @@ int main(){
         printf("\n");
     }
 
-    printf("\n\nPelo criterio de Kiser o valor de 'm' em R é %d", m);
+    //M do metodo de Kaiser
+    printf("\n\nPelo criterio de Kiser o valor de 'm' em R eh %d", m);
 
     return 0;
 }
